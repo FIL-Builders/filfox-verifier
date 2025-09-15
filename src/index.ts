1d4ac1645325b368f82255d1a514022ca715c179
nijoe1 <nick@fil.builders>
2025-09-15 11:01:27 +0300

    fix: update version to be read from package.json

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+import { readFileSync } from "fs";
+import { join } from "path";
 import { verifyFilfoxCommand } from "./commands/verify-filfox";
 
 const program = new Command();
 
+// Get package.json path and read version
+const packageJsonPath = join(__dirname, "../package.json");
+const packageJson = JSON.parse(readFileSync(packageJsonPath, "utf8"));
+
 program
   .name("filfox-verifier")
   .description("CLI tool for verifying smart contracts on Filfox")
-  .version("1.0.0");
+  .version(packageJson.version);
 
 program.addCommand(verifyFilfoxCommand);
 if (require.main === module) {
